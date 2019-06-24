1998724e3e39b94ff5c1e768b37ad77fcd9f2fff
wafik111 <eng.ahmedwafik@gmail.com>
2019-06-24 16:22:57 +0200

    modal

--- public/js/penalty/penaltyOptions.js
+++ public/js/penalty/penaltyOptions.js
@@ -1,26 +1,15 @@
 
-
-
-
-
+let btn = document.getElementById('penaltyDeleted');
+let table = document.getElementsByTagName('table')[0];
+ajax = new XMLHttpRequest();
 
 
 //delete
-ajax = new XMLHttpRequest();
 table.onclick = function(event){
     if(event.target.name=="penaltyDeleted"){
+        value = event.target.getAttribute('value');
+        console.log(value);
         row = event.target.parentElement.parentElement;
-        ajax.open("DELETE","http://localhost:8888/penalty/delete/"+ event.target.getAttribute('value'));
-        ajax.send();
-        ajax.onreadystatechange = ()=>{
-            if(ajax.readyState === 4 && ajax.status === 200){
-                response = JSON.parse(ajax.responseText);
-                console.log(response);
-                if(response.success){
-                    row.remove();
-                }
-            }
-        }
         
     }else if(event.target.name=="penaltyApproved"){
         row = event.target.parentElement.parentElement;
@@ -41,3 +30,16 @@ table.onclick = function(event){
     }
 }
       
+btn.onclick = function(){
+    ajax.open("DELETE","http://localhost:8888/penalty/delete/"+ value);
+        ajax.send();
+        ajax.onreadystatechange = ()=>{
+            if(ajax.readyState === 4 && ajax.status === 200){
+                response = JSON.parse(ajax.responseText);
+                console.log(response);
+                if(response.success){
+                    row.remove();
+                }
+            }
+        }
+}
